15fc248f5e2bea7be02cf2fe4de3192b7108dc8c
Harmonico Lucas <harmonico6lucas3@gmail.com>
2025-11-04 10:00:00 +0200

    Adiciona DTO AuthResponse

---
+++ XXX/backend/src/main/java/com/juriconnect/dto/AuthResponse.java
@@ -0,0 +1,15 @@
+package com.juriconnect.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String email;
+    private String role;
+    private Long userId;
+    private String fullName;
+}
+
