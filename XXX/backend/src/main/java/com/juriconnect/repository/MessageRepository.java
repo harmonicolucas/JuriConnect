8f4ce0bbe898887d9c768ce43ec93fc1b3cf253f
Gracio Kabongo Lumbala <graciolumbala01@gmail.com>
2025-11-16 19:16:59 +0200

    MessageRepository commit

---
+++ XXX/backend/src/main/java/com/juriconnect/repository/MessageRepository.java
@@ -0,0 +1,14 @@
+package com.juriconnect.repository;
+
+import com.juriconnect.model.LegalCase;
+import com.juriconnect.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> findByLegalCaseOrderBySentAtAsc(LegalCase legalCase);
+}
+
